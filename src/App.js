1ea0a1af3e27c217c0c8621b882d353ead3adde3
rodrigoquaresma <rodrigo.quaresma@resultadosdigitais.com.br>
2019-06-05 08:37:11 -0300

    bootstrap installed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-muted">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
