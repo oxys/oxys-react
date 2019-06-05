c029b6c0a7c86ea36b2bde756640355c883fb34b
rodrigoquaresma <rodrigo.quaresma@resultadosdigitais.com.br>
2019-06-05 10:04:22 -0300

    add the right logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo2 from './logo.svg';
+import logo from './brand-v2.1.svg';
 import './App.scss';
 
 
@@ -7,18 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-muted">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 22
-        </a>
+        <img src={logo} alt="logo" className="logo-header" />
       </header>
     </div>
   );
