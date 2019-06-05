e0c3b4d2e7fce177e80ff77362e3f58390ac0c2f
rodrigoquaresma <rodrigo.quaresma@resultadosdigitais.com.br>
2019-06-05 10:45:41 -0300

    new logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo2 from './logo.svg';
 import logo from './brand-v2.1.svg';
+import logo22 from './brand-v2.2.svg';
+import logo23 from './brand-v2.3.svg';
 import './App.scss';
 
 
@@ -8,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="logo" className="logo-header" />
+        <img src={logo23} alt="logo" className="logo-header" />
       </header>
     </div>
   );
