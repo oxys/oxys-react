7b3546e4e5fdf7ce7e3e5653be6d1e72dba6ce6e
rodrigoquaresma <rodrigo.quaresma@resultadosdigitais.com.br>
2019-10-16 12:58:27 -0300

    trying to fix

--- src/formtestsNew.js
+++ src/formtestsNew.js
@@ -5,7 +5,7 @@ function FormtestsNew() {
   return (
     <div className="formtestes">
       <h2>form bricks</h2>
-      <div className="novoform">
+      <div className="showtheform novoform">
         <div role="main" id="modelo-simples-de-formulario-embedado-543e67a4ae1860582e95"></div>
         <script type="text/javascript" src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"></script>
         <script type="text/javascript"> new RDStationForms('modelo-simples-de-formulario-embedado-543e67a4ae1860582e95', 'UA-47500039-1').createForm();</script>
--- src/formtestsOld.js
+++ src/formtestsOld.js
@@ -6,7 +6,7 @@ class FormtestsOld extends React.Component {
     return (
       <div className="formtestes">
         <h2>form lucy</h2>
-        <div className="formantigo">
+        <div className="showtheform formantigo">
           <div role="main" id="dfgdfgdf-86635962aad2878f482e"></div>
           <script type="text/javascript" src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"></script>
           <script type="text/javascript">
