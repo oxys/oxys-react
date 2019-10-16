e7ad16043c201a4061042f7a69107c055196cd29
rodrigoquaresma <rodrigo.quaresma@resultadosdigitais.com.br>
2019-10-16 12:32:51 -0300

    fiz routes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import formtestsOld from './formtestsOld';
 import formtestsNew from './formtestsNew';
-import * as serviceWorker from './serviceWorker';
+import Notfound from './notfound'
+// import * as serviceWorker from './serviceWorker';
 
 const routing = (
   <Router>
-    <div>
+    <Switch>
       <Route exact path="/" component={App} />
       <Route path="/formtests-old" component={formtestsOld} />
       <Route path="/formtests-new" component={formtestsNew} />
-    </div>
+      <Route component={Notfound} />
+    </Switch>
   </Router>
 )
 
@@ -22,4 +24,4 @@ ReactDOM.render(routing, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
