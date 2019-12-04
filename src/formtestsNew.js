3af4536e05df80fc82209cbe921c963ca1084e24
rodrigoquaresma <rodrigo.quaresma@resultadosdigitais.com.br>
2019-12-04 07:57:52 -0300

    review fix

--- src/formtestsNew.js
+++ src/formtestsNew.js
@@ -7,8 +7,6 @@ function FormtestsNew() {
       <h2>form bricks</h2>
       <div className="showtheform novoform">
         <div role="main" id="modelo-simples-de-formulario-embedado-543e67a4ae1860582e95"></div>
-        <script type="text/javascript" src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"></script>
-        <script type="text/javascript"> new RDStationForms('modelo-simples-de-formulario-embedado-543e67a4ae1860582e95', 'UA-47500039-1').createForm();</script>
       </div>
     </div>
   );
--- src/formtestsOld.js
+++ src/formtestsOld.js
@@ -8,10 +8,6 @@ class FormtestsOld extends React.Component {
         <h2>form lucy</h2>
         <div className="showtheform formantigo">
           <div role="main" id="dfgdfgdf-86635962aad2878f482e"></div>
-          <script type="text/javascript" src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"></script>
-          <script type="text/javascript">
-            new RDStationForms('dfgdfgdf-86635962aad2878f482e-html', 'UA-47500039-1').createForm();
-          </script>
         </div>
       </div>
     );
